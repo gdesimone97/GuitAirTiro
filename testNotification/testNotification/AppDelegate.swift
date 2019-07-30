b840dcaec6658cf2d8a8badcabeebc74185ec485
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2019-07-30 11:48:44 +0200

    Update AppDelegate.swift

--- testNotification/testNotification/AppDelegate.swift
+++ testNotification/testNotification/AppDelegate.swift
@@ -12,59 +12,58 @@ import CloudKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
     let notificationCenter = UNUserNotificationCenter.current()
-
+    let CATEGORY: String = "INVITATION"
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         // Autorizzazioni
-        notificationCenter.requestAuthorization(options: [.alert , .sound, .badge])
-        { (granted, error) in /* funzionalità in base all'autorizzazione */ }
+        notificationCenter.requestAuthorization(options: [.alert , .sound]) { (granted, error) in /* funzionalità in base all'autorizzazione */ }
         notificationCenter.getNotificationSettings { (settings) in
             guard settings.authorizationStatus == .authorized else { return }
+            DispatchQueue.main.async {
+                // Registrazione per le push notification
+                application.registerForRemoteNotifications()
+            }
             self.notificationCategory()
             if settings.alertSetting == .enabled {
                 /* Fai qualcosa se hai i permessi per l'allert */
             }
             else { /* Fai altro */ }
         }
-        // Fine autorizzazioni
         
+        // Fine autorizzazioni
         notificationCenter.delegate = self
         
         return true
     }
-
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
     }
-
+    
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
-
+    
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
-
+    
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
-
+    
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        
-    }
-    
     func notificationCategory() {
-        let CATEGORY: String = "INVITATION"
-        let gamerTag = "" /* Da completare */
+        
         let acceptAction = UNNotificationAction(identifier: "ACCEPT_ACTION", title: "Accept Invite", options: UNNotificationActionOptions.init(rawValue: 0))
         let declineAction = UNNotificationAction(identifier: "DECLINE_ACTION", title: "Decline Invite", options: UNNotificationActionOptions(rawValue: 0))
         let inviteCategory = UNNotificationCategory(identifier: CATEGORY, actions: [acceptAction,declineAction], intentIdentifiers: [], hiddenPreviewsBodyPlaceholder: "", options: .customDismissAction)
@@ -72,6 +71,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
     }
     
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        
+        let tokenParts = deviceToken.map { data in String(format: "%02.2hhx", data) }
+        let token = tokenParts.joined()
+        print("Device Token: \(token)")
+        
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Push notification non attive")
+    }
     
 }
 
@@ -92,4 +102,12 @@ extension AppDelegate: UNUserNotificationCenterDelegate {
         completionHandler()
     }
     
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        if notification.request.content.categoryIdentifier == CATEGORY {
+            let gamerTag = notification.request.content.userInfo["GAMERTAG"]
+            completionHandler(.sound)
+            return
+        }
+        completionHandler(UNNotificationPresentationOptions(rawValue: 0))
+    }
 }
