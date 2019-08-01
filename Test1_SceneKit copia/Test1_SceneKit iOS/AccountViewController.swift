62a7d887e202128d26d6ae0fa0717421d6240e8a
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2019-08-01 16:38:12 +0200

    Create AccountViewController.swift

---
+++ Test1_SceneKit copia/Test1_SceneKit iOS/AccountViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AccountViewController.swift
+//  Test1_SceneKit iOS
+//
+//  Created by Giuseppe De Simone on 01/08/2019.
+//  Copyright © 2019 Gennaro Giaquinto. All rights reserved.
+//
+
+import UIKit
+
+class AccountViewController: UIViewController {
+
+    let userDefaults = UserDefaults.standard
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if  !userDefaults.bool(forKey: LOGIN) {
+            self.performSegue(withIdentifier: "login_view", sender: nil)
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
