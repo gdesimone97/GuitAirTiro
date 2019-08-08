f325409bd9fb88e79f0f80ac48759d76c6269397
Mario <mariodesio1@gmail.com>
2019-08-08 13:18:20 +0200

    Swipe implementation in guitar selecting

--- Test1_SceneKit copia/Test1_SceneKit iOS/GuitarSelectedViewController.swift
+++ Test1_SceneKit copia/Test1_SceneKit iOS/GuitarSelectedViewController.swift
@@ -15,39 +15,73 @@ class GuitarSelectedViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var guitarLabel: UILabel!
     
+    // swipe variables
+    @IBOutlet var leftSwipe: UISwipeGestureRecognizer!
+    @IBOutlet var rightSwipe: UISwipeGestureRecognizer!
+    
+    let electricImage : UIImage = (UIImage(named: "electric"))!
+    let acousticImage : UIImage = (UIImage(named: "acoustic"))!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        imageView.isUserInteractionEnabled = true
         
+        //swipe directions
+        leftSwipe.direction = UISwipeGestureRecognizer.Direction.left
+        rightSwipe.direction = UISwipeGestureRecognizer.Direction.right
+        
+        
+        imageView!.image = acousticImage
+        guitarLabel!.text = "Acoustic"
     }
     
-    @IBAction func dxButton(_ sender: Any) {
-        let guitar = UserDefaults.getGuitar(forKey: GUITAR)
-            if guitar == TypeOfGuitar.classic {
-                if let device = session.showConnectedDevices() {
-                    session.sendSignal(device[0], message: SignalCode.showElectricGuitar)
+    @IBAction func leftSwipePerformed(_ sender: UISwipeGestureRecognizer) {
+        if sender.state == .ended {
+            let guitar = UserDefaults.getGuitar(forKey: GUITAR)
+                    if guitar == TypeOfGuitar.classic {
+                        if let device = session.showConnectedDevices() {
+                            session.sendSignal(device[0], message: SignalCode.showElectricGuitar)
+                        }
+                        UserDefaults.setGuitar(guitar: TypeOfGuitar.electric, forKey: GUITAR)
+                        imageView!.image = electricImage
+                        guitarLabel!.text = "Electric"
+                    }
+                    else if guitar == TypeOfGuitar.electric {
+                        if let device = session.showConnectedDevices() {
+                            session.sendSignal(device[0], message: SignalCode.showAcousticGuitar)
+                        }
+                        UserDefaults.setGuitar(guitar: TypeOfGuitar.classic, forKey: GUITAR)
+                        imageView!.image = acousticImage
+                        guitarLabel!.text = "Acoustic"
+                    }
                 }
-                UserDefaults.setGuitar(guitar: TypeOfGuitar.electric, forKey: GUITAR)
             }
-            else if guitar == TypeOfGuitar.electric {
-                if let device = session.showConnectedDevices() {
-                    session.sendSignal(device[0], message: SignalCode.showAcousticGuitar)
-                }
-                UserDefaults.setGuitar(guitar: TypeOfGuitar.classic, forKey: GUITAR)
-            }
-    }
-    @IBAction func sxButton(_ sender: Any) {
-        let guitar = UserDefaults.getGuitar(forKey: GUITAR)
-        if guitar == TypeOfGuitar.classic {
-            if let device = session.showConnectedDevices() {
-                session.sendSignal(device[0], message: SignalCode.showElectricGuitar)
-            }
-            UserDefaults.setGuitar(guitar: TypeOfGuitar.electric, forKey: GUITAR)
+    
+    
+    
+    @IBAction func rightSwipePerformed(_ sender: UISwipeGestureRecognizer) {
+        if sender.state == .ended {
+                    let guitar = UserDefaults.getGuitar(forKey: GUITAR)
+                        if guitar == TypeOfGuitar.classic {
+                            if let device = session.showConnectedDevices() {
+                                session.sendSignal(device[0], message: SignalCode.showElectricGuitar)
+                            }
+                            UserDefaults.setGuitar(guitar: TypeOfGuitar.electric, forKey: GUITAR)
+                            imageView!.image = electricImage
+                            guitarLabel!.text = "Electric"
+                        }
+                        else if guitar == TypeOfGuitar.electric {
+                            if let device = session.showConnectedDevices() {
+                                session.sendSignal(device[0], message: SignalCode.showAcousticGuitar)
+                            }
+                            UserDefaults.setGuitar(guitar: TypeOfGuitar.classic, forKey: GUITAR)
+                            imageView!.image = acousticImage
+                            guitarLabel!.text = "Acoustic"
+                        }
+            
+            
         }
-        else if guitar == TypeOfGuitar.electric {
-            if let device = session.showConnectedDevices() {
-                session.sendSignal(device[0], message: SignalCode.showAcousticGuitar)
-            }
-            UserDefaults.setGuitar(guitar: TypeOfGuitar.classic, forKey: GUITAR)
-        }
     }
+    
+
 }
