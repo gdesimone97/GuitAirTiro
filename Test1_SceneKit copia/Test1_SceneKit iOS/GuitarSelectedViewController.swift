204221e6f8b56365b0560eaf66a130b325d11a2e
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2019-08-20 22:21:43 +0200

    risolto bug chitarra

--- Test1_SceneKit copia/Test1_SceneKit iOS/GuitarSelectedViewController.swift
+++ Test1_SceneKit copia/Test1_SceneKit iOS/GuitarSelectedViewController.swift
@@ -29,10 +29,19 @@ class GuitarSelectedViewController: UIViewController {
         //swipe directions
         leftSwipe.direction = UISwipeGestureRecognizer.Direction.left
         rightSwipe.direction = UISwipeGestureRecognizer.Direction.right
+        let guitar = UserDefaults.getGuitar(forKey: GUITAR)
+        switch guitar {
+        case .classic?:
+            imageView.image = acousticImage
+            guitarLabel!.text = "Acoustic"
+        case .electric?:
+            imageView.image = electricImage
+            guitarLabel!.text = "Electric"
+        default:
+            imageView.image = acousticImage
+        }
         
         
-        imageView!.image = acousticImage
-        guitarLabel!.text = "Acoustic"
     }
     
     @IBAction func leftSwipePerformed(_ sender: UISwipeGestureRecognizer) {
