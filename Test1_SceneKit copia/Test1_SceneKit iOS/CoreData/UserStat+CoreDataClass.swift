97907575b420d91aff8aba07f4e53aa3ae0bf575
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2019-08-21 13:32:58 +0200

    core data

---
+++ Test1_SceneKit copia/Test1_SceneKit iOS/CoreData/UserStat+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  UserStat+CoreDataClass.swift
+//  
+//
+//  Created by Giuseppe De Simone on 21/08/2019.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(UserStat)
+public class UserStat: NSManagedObject {
+
+}
