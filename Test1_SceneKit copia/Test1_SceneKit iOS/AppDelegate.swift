8a683005902508115b15d6c01f0702cd29b61d18
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2019-08-21 15:46:39 +0200

    Revert "core data"
    This reverts commit 97907575b420d91aff8aba07f4e53aa3ae0bf575.

--- Test1_SceneKit copia/Test1_SceneKit iOS/AppDelegate.swift
+++ Test1_SceneKit copia/Test1_SceneKit iOS/AppDelegate.swift
@@ -19,32 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     let NOTATION_KEY = "PreferredNotation";
     let notificationCenter = UNUserNotificationCenter.current()
     let CATEGORY: String = "INVITATION"
-
     let sessionTv = SessionManager.share
 
-
-    lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: appModel)
-        container.loadPersistentStores(completionHandler: {(storeDescription, error) in
-            if let error = error as NSError? {
-                fatalError("Errore sconosciuto \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-
-    func saveContext() {
-        let context = persistentContainer.viewContext
-        if context.hasChanges{
-            do {
-                try context.save()
-            }catch {
-                let nserror = error as NSError
-                fatalError("Errore nel salvataggio \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 
         // Override point for customization after application launch.
--- Test1_SceneKit copia/Test1_SceneKit iOS/CoreData/persistenceManager.swift
+++
@@ -1,9 +0,0 @@
-//
-//  persistenceManager.swift
-//  Test1_SceneKit iOS
-//
-//  Created by Giuseppe De Simone on 21/08/2019.
-//  Copyright © 2019 Gennaro Giaquinto. All rights reserved.
-//
-
-import Foundation
