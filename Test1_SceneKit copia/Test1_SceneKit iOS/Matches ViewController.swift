e58391fc05128ae4cd4be48aad583fd2586ab635
Mario <mariodesio1@gmail.com>
2019-08-23 13:19:58 +0200

    Storyboard

---
+++ Test1_SceneKit copia/Test1_SceneKit iOS/Matches ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  Matches ViewController.swift
+//  Test1_SceneKit iOS
+//
+//  Created by Mario De Sio on 23/08/2019.
+//  Copyright © 2019 Gennaro Giaquinto. All rights reserved.
+//
+
+import UIKit
+
+class Matches_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Test1_SceneKit copia/Test1_SceneKit iOS/Pending Request ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  Pending Request ViewController.swift
+//  Test1_SceneKit iOS
+//
+//  Created by Mario De Sio on 23/08/2019.
+//  Copyright © 2019 Gennaro Giaquinto. All rights reserved.
+//
+
+import UIKit
+
+class Pending_Request_ViewController: UIViewController {
+
+    @IBOutlet weak var innerView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        innerView.layer.cornerRadius = 14
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
