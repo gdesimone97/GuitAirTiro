5a18689eec7e72ecf3b7142ce73a3b95d4339efe
Mario <mariodesio1@gmail.com>
2019-08-26 17:16:45 +0200

    Main storyboard

--- Test1_SceneKit copia/Test1_SceneKit iOS/Multiplayer TableViewController.swift
+++ Test1_SceneKit copia/Test1_SceneKit iOS/Multiplayer TableViewController.swift
@@ -10,28 +10,30 @@ import UIKit
 
 class Multiplayer_TableViewController: UITableViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var resultSearchView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
+    
+    
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
-
+/*
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return
     }
-
+*/
     /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
