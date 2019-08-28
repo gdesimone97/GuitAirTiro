f589c7952fb5be3b1168c87b46ebe3400392511b
christian <chsmrn@gmail.com>
2019-08-28 16:23:37 +0200

    core

---
+++ Test1_SceneKit copia/Test1_SceneKit iOS/Core Data/Games+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Games+CoreDataClass.swift
+//  Test1_SceneKit iOS
+//
+//  Created by Christian Marino on 28/08/2019.
+//  Copyright © 2019 Gennaro Giaquinto. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Games)
+public class Games: NSManagedObject {
+
+}
---
+++ Test1_SceneKit copia/Test1_SceneKit iOS/Core Data/Games+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  Games+CoreDataProperties.swift
+//  Test1_SceneKit iOS
+//
+//  Created by Christian Marino on 28/08/2019.
+//  Copyright © 2019 Gennaro Giaquinto. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Games {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Games> {
+        return NSFetchRequest<Games>(entityName: "Games")
+    }
+
+    @NSManaged public var gamesSerialized: NSData?
+    @NSManaged public var last_server_read: NSDate?
+
+}
